3cfccc359fceb1400f08f7241f0984705d2bf347
Geedoo Kanda <geedookanda06@gmail.com>
2023-07-15 00:57:26 +0100

    gestion du systeme auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('adresse')->nullable();
+            $table->string('acces')->nullable();
+            $table->string('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
